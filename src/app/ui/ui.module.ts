66c935eac02fcb8464a2d91a18ecafc0bde016ff
utheron <aaron.khochareun@gmail.com>
2021-11-16 09:27:28 +0100

    TP Ui2

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OrdersModule } from '../orders/orders.module';
 import { UiComponent } from './components/ui/ui.component';
+import { Ui2Component } from './components/ui2/ui2.component';
 
 @NgModule({
-  declarations: [UiComponent],
+  declarations: [UiComponent, Ui2Component],
   imports: [CommonModule, OrdersModule],
-  exports: [UiComponent],
+  exports: [UiComponent, Ui2Component],
 })
 export class UiModule {}
