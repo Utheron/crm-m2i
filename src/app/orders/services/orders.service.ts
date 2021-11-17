e7e608a92fbc7f011d4bd77da2613f6b4d64da1c
utheron <aaron.khochareun@gmail.com>
2021-11-17 11:07:40 +0100

    added observable

--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -1,9 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from 'src/app/core/models/order';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrdersService {
-  constructor(private http: HttpClient) {}
+  private urlApi = environment.urlApi;
+  public collection$: Observable<Order[]>;
+
+  constructor(private http: HttpClient) {
+    this.collection$ = this.http.get<Order[]>(`${this.urlApi}/orders`);
+  }
 }
