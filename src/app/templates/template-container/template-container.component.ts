ca831d90b5159f3c4c6d346941fa7edb6d29b141
utheron <aaron.khochareun@gmail.com>
2021-11-17 15:04:05 +0100

    changed template selection

--- src/app/templates/template-container/template-container.component.ts
+++ src/app/templates/template-container/template-container.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TemplateContainerComponent implements OnInit {
   @Input() public title!: string;
+
   constructor() {
     this.title = 'Ohhhhh le joli title';
   }
